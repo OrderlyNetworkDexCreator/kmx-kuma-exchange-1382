07d40b0eebe4c4d1551423de27d83402b974a28e
Mario Reder <mario.reder@pm.me>
2025-09-28 05:57:45 +0200

    Setup DEX with workflow files and configuration

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 window.__RUNTIME_CONFIG__ = {
-  "VITE_ORDERLY_BROKER_ID": "demo",
+  "VITE_ORDERLY_BROKER_ID": "kmx-exchange",
   "VITE_ORDERLY_BROKER_NAME": "KMX - Kuma Exchange",
   "VITE_DISABLE_MAINNET": "false",
   "VITE_DISABLE_TESTNET": "true",
